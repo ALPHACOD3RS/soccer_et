79376a9b5931fb477aff91c604ff55f56ecc2e93
ALPHACO3RS <naolketema55@gmail.com>
2022-02-07 11:16:04 +0600

    in this file nothing aded lol

---
+++ lib/view/app_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:live_score_app/model/news_model.dart';
+
+class AppBar extends StatefulWidget {
+  const AppBar({Key? key}) : super(key: key);
+
+  @override
+  _AppBarState createState() => _AppBarState();
+}
+
+class _AppBarState extends State<AppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(),
+    );
+  }
+}
